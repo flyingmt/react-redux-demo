6c8c11eb1023dfcafa712ce0f21f9533792a4267
Jun-Hee Lee <flyingmt@gmail.com>
2020-05-13 12:15:26 +0900

    added hooks

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import store from './redux/store';
 
 import './App.css';
 import CakeContainer from './components/CakeContainers';
+import HooksCakeContainer from './components/HooksCakeContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HooksCakeContainer />
         <CakeContainer />
       </div>
     </Provider>
